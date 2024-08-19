2c47d75c42fc4f2f1b6eeee6a53379ddd518c667
劉清揚 <2465199797@qq.com>
2024-08-19 16:36:05 +0800

    测试3

--- _site/module_xiaowanle/page/page_music2/js/musicList.js
+++ _site/module_xiaowanle/page/page_music2/js/musicList.js
@@ -511,6 +511,18 @@ const musicList3 = [
     des: "",
     lyricstype: false
   },
+  {
+    title: "测试3",
+    author: "福禄寿",
+    name_path: "测试3.MP3",
+    type: TYPE_3,
+    type_path: FILE_MUSIC_EMO,
+    imgPath: "./imgs/17.jpg",
+    time: '05:24',
+    lyrics: false,
+    des: "",
+    lyricstype: false
+  },
 ]
 
 //经典
